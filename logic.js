ad3cae8680d83fbf9e52da45ede541dc40540f26
DDD <DDD@IT471NBMSK.ozon>
2019-12-01 16:59:52 +0300

    init

---
+++ logic.js
@@ -0,0 +1,10 @@
+const tokens = {
+    access: "5e3753525d33428af5079d49a986e9f350433dd22ecad9034e406741a65cd5e2",
+    secret: "2a08d6f19874b56e419b1f5ba4eb77555f664ebd9d36a655c3cf93f232758f6c",
+}
+
+console.log(tokens.access);
+
+const axios = require('axios').default;
+
+axios.get('https://api.usplash.com');
